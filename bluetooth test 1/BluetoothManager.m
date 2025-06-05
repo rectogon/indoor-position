dd3dd53388bd3b3e8442687f7c26da3365acedb4
rectogon <125447288+rectogon@users.noreply.github.com>
2025-06-05 16:01:51 +0700

    Update BluetoothManager.m

--- bluetooth test 1/BluetoothManager.m
+++ bluetooth test 1/BluetoothManager.m
@@ -1,53 +1,43 @@
 #import "test-Bridging-Header.h"
-#import "test-Swift.h"  // Replace with real name
+#import "test-Swift.h"  // Replace with your actual Swift header name
 
 @interface BluetoothBridge () <BluetoothScanDelegate>
-@end
-
-#import <Foundation/Foundation.h>
-
-@interface BluetoothBridge ()
+@property (nonatomic, strong) BluetoothManager *manager;
 @property (nonatomic, strong) NSDictionary *lastDevice;
 @end
 
-@implementation BluetoothBridge {
-    BluetoothManager *_manager;
-}
+@implementation BluetoothBridge
 
 - (instancetype)init {
     self = [super init];
     if (self) {
         _manager = [[BluetoothManager alloc] init];
         _manager.delegate = self;
+        NSLog(@"BluetoothBridge initialized with manager: %@", _manager);
     }
     return self;
 }
 
 - (void)startBluetoothScan {
+    NSLog(@"BluetoothBridge: Starting scan...");
     [_manager startScan];
 }
 
 - (void)stopBluetoothScan {
+    NSLog(@"BluetoothBridge: Stopping scan...");
     [_manager stopScan];
 }
 
+// This method will be called by Swift BluetoothManager
 - (void)didDiscoverDeviceWithName:(NSString *)name
                              uuid:(NSString *)uuid
                              rssi:(NSInteger)rssi
                             major:(NSInteger)major
                             minor:(NSInteger)minor {
     
-    NSDictionary *userInfo = @{
-        @"name": name ?: @"Unknown",
-        @"uuid": uuid,
-        @"rssi": @(rssi),
-        @"major": @(major),
-        @"minor": @(minor)
-    };
-
-    [[NSNotificationCenter defaultCenter] postNotificationName:@"BluetoothDeviceDiscovered"
-                                                        object:nil
-                                                      userInfo:userInfo];
+    NSLog(@"BluetoothBridge: Device discovered - Name: %@, UUID: %@, RSSI: %ld", 
+          name, uuid, (long)rssi);
+    
     self.lastDevice = @{
         @"name": name ?: @"Unknown",
         @"uuid": uuid ?: @"",
@@ -55,9 +45,8 @@
         @"major": @(major),
         @"minor": @(minor)
     };
-
-    // ✅ You can store it or send it to Python
-
+    
+    NSLog(@"BluetoothBridge: lastDevice set to: %@", self.lastDevice);
 }
 
 - (void)connectToDeviceWithUUID:(NSString *)uuid {
@@ -65,6 +54,7 @@
 }
 
 - (NSDictionary *)getLastDiscoveredDevice {
+    NSLog(@"BluetoothBridge: getLastDiscoveredDevice called, returning: %@", self.lastDevice);
     return self.lastDevice;
 }
 
